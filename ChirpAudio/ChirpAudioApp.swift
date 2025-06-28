0f30cde5b4776f51d6f96be27de03494e22b7178
Karim Abdul <kforkarim@gmail.com>
2025-06-28 15:02:06 -0500

    Initial Commit

---
+++ ChirpAudio/ChirpAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChirpAudioApp.swift
+//  ChirpAudio
+//
+//  Created by Karim Abdul on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ChirpAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChirpAudioTests/ChirpAudioTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChirpAudioTests.swift
+//  ChirpAudioTests
+//
+//  Created by Karim Abdul on 6/28/25.
+//
+
+import Testing
+@testable import ChirpAudio
+
+struct ChirpAudioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
